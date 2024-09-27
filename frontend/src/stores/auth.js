b413d50ed82d274c3e875b7dae3deadb5d998dbd
Nam Nguyen <101324693+nnguyennam2003@users.noreply.github.com>
2024-09-27 15:35:05 +0700

    [FE][Feature][Login] Create login social feature (#25)
    * clone file login feature to newLogin branch
    
    * fix bug rebase to dev done

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,13 +1,15 @@
 import { defineStore } from 'pinia'
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from '../services/firebaseConfig.js'
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { HEADERS_DEFAULT } from '@constants/header.constant.js';
+import { API_METHOD } from '@constants/api-method.constant.js';
 
 export const useAuthStore = defineStore('auth', () => {
     // States
     const user = ref({})
-    const loading = ref(false);
     const error = ref(null);
+    const token = ref(null);
 
     // Actions
     const googleSignIn = async () => {
@@ -15,19 +17,82 @@ export const useAuthStore = defineStore('auth', () => {
         try {
             const result = await signInWithPopup(auth, provider);
             user.value = result.user;
+            token.value = await result.user.getIdToken();
             console.log("User info:", user.value);
         } catch (err) {
             error.value = err.message;
             console.error("Error during Google login:", error.value);
-        } finally {
-            loading.value = false;
+        }
+    }
+
+    // Gửi idToken cho backend ( Pending )
+    const sendTokenToBackend = async () => {
+        if (token.value) {
+            try {
+                const response = await fetch('', { // đợi api của backend
+                    method: API_METHOD.POST,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ idToken: token.value }),
+                });
+                const data = await response.json();
+                console.log('Response from backend:', data);
+            } catch (error) {
+                console.error('Error sending token to backend:', error);
+            }
+        } else {
+            console.error('No token available to send to backend');
+        }
+    };
+
+    onMounted(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                user.value = currentUser;
+                currentUser.getIdToken().then((idToken) => {
+                    token.value = idToken;
+                });
+            } else {
+                user.value = null;
+                token.value = null;
+            }
+        });
+        return () => {
+            unsubscribe();
+        };
+    });
+
+    const logout = async () => {
+        await auth.signOut()
+        user.value = {};
+    };
+
+    // Chưa có api login từ backend ( Pending )
+    const loginWithEmail = async (values) => {
+        try {
+            const response = await fetch('', {
+                method: 'POST',
+                headers: HEADERS_DEFAULT,
+                body: JSON.stringify(values),
+                credentials: 'include'
+            });
+    
+            const data = await response.json();
+            if(data.error) {
+                alert(data.error)
+            }
+            user.value = data
+            console.log('data', data)
+        } catch (error) {
+            console.log(error);
         }
     }
 
     return {
         // states
-        user, loading, error,
+        user, error,
         // actions
-        googleSignIn
+        googleSignIn, sendTokenToBackend, logout, loginWithEmail
     }
 })
