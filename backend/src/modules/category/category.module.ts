cbea0da189aa2aa468e84b2e4d3698bc599dcd91
HuyHue <dqh1005@gmail.com>
2024-10-01 08:57:49 +0700

    feat(86eq9ff1e): API upload video

---
+++ backend/src/modules/category/category.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from '@/entities/category.entity';
+import { CategoryRepository } from './caregory.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category])],
+  controllers: [CategoryController],
+  providers: [CategoryService, CategoryRepository],
+  exports: [CategoryService,CategoryRepository],
+})
+export class CategoryModule {}
--- backend/src/modules/video/video.module.ts
+++ backend/src/modules/video/video.module.ts
@@ -5,20 +5,21 @@ import { VideoController } from './video.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Video } from '@/entities/video.entity';
 import { VideoRepository } from './video.repository';
-import { CommentModule } from '../comment/comment.module';
+import { AwsS3Service } from '@/shared/services/aws-s3.service';
+import { GeneratorService } from '@/shared/services/generator.service';
+import { CategoryModule } from '../category/category.module';
+import { CategoryService } from '../category/category.service';
+import { VimeoService } from '@/shared/services/vimeo.service';
 import { UserModule } from '../user/user.module';
-import { ChannelModule } from '../channel/channel.module';
 import { JwtService } from '@nestjs/jwt';
 
+
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Video]),
-    CommentModule,
-    WatchingVideoHistoryModule,
-    UserModule,
-    ChannelModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Video]), UserModule, CategoryModule],
   controllers: [VideoController],
-  providers: [VideoService, VideoRepository, JwtService],
+  providers: [VideoService, VideoRepository, JwtService,
+    AwsS3Service, GeneratorService, CategoryService,
+    VimeoService
+  ],
 })
 export class VideoModule {}
