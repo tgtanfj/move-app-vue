506e0d2ce63646d520dfe7fee45709e5e1f58886
Đạt <datlowashere@gmail.com>
2024-10-01 14:56:15 +0700

    [Mobile][Sign Up][Feature] Update constants

--- mobile/lib/constants/constants.dart
+++ mobile/lib/constants/constants.dart
@@ -1,4 +1,9 @@
 class Constants {
+  static const String featured = 'Featured';
+  static const String views = 'views';
+  static const String category = 'Category';
+  static const String viewAll = 'View All';
+  static const String videosYouMayLike = 'Videos you may like';
   static const String login = "Log In";
   static const String signUp = "Sign Up";
   static const String signUpWithGoogle = "Sign up with Google";
