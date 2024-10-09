41b09bba7468529106bc681edef6aa75ed97d6c0
m1nhnguy3n <minhdacamst@gmail.com>
2024-10-09 16:07:04 +0700

    feat(86eqc8h6b): Add payment method

--- backend/src/modules/stripe/stripe.controller.ts
+++ backend/src/modules/stripe/stripe.controller.ts
@@ -1,10 +1,24 @@
-/* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { User } from '@/shared/decorators/user.decorator';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AttachPaymentMethodDto } from './dto/attach-payment-method.dto';
 import { StripeService } from './stripe.service';
+import { JwtAuthGuard } from '@/shared/guards';
 
 @Controller('stripe')
 @ApiTags('stripe')
 export class StripeController {
   constructor(private stripeService: StripeService) {}
+
+  @Get('list-cards')
+  @UseGuards(JwtAuthGuard)
+  async getAllPaymentMethod(@User() user) {
+    return this.stripeService.listPaymentMethod(user.stripeId);
+  }
+
+  @Post('attach-card')
+  @UseGuards(JwtAuthGuard)
+  async attachPaymentMethod(@User() user, @Body() addPaymentMethod: AttachPaymentMethodDto) {
+    return this.stripeService.attachPaymentMethod(user.id, addPaymentMethod);
+  }
 }
--- backend/src/modules/stripe/stripe.service.ts
+++ backend/src/modules/stripe/stripe.service.ts
@@ -1,13 +1,14 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { ERRORS_DICTIONARY } from '@/shared/constraints/error-dictionary.constraint';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import { AttachPaymentMethodDto } from './dto/attach-payment-method.dto';
 
 @Injectable()
 export class StripeService {
   private stripe: Stripe;
   constructor(private configService: ConfigService) {
-    this.stripe = new Stripe(configService.get('STRIPE_SECRET_KEY'), {
+    this.stripe = new Stripe(this.configService.get<string>('STRIPE_SECRET_KEY'), {
       apiVersion: '2024-06-20',
     });
   }
@@ -16,5 +17,39 @@ export class StripeService {
     return this.stripe.customers.create({
       email,
     });
+  }
+
+  async listPaymentMethod(stripeId: string) {
+    const paymentMethods = await this.stripe.customers.listPaymentMethods(stripeId, {
+      type: 'card',
+    });
+
+    const paymentMethodFiltered = paymentMethods.data.map((paymentMethod) => ({
+      id: paymentMethod.id,
+      type: paymentMethod.type,
+      card: {
+        brand: paymentMethod.card.brand,
+        last4: paymentMethod.card.last4,
+        exp_month: paymentMethod.card.exp_month,
+        exp_year: paymentMethod.card.exp_year,
+      },
+      name: paymentMethod.billing_details.name,
+    }));
+
+    return paymentMethodFiltered;
+  }
+
+  async attachPaymentMethod(customerId: string, addPaymentMethod: AttachPaymentMethodDto) {
+    const { paymentMethodId } = addPaymentMethod;
+
+    await this.stripe.setupIntents
+      .create({
+        customer: customerId,
+        payment_method: paymentMethodId,
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new BadRequestException(ERRORS_DICTIONARY.ADD_PAYMENT_METHOD_FAIL);
+      });
   }
 }
