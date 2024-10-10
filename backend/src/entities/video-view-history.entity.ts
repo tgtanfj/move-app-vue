592a4304448870078bd56b6ddf510fafb7ced3e0
AnhNN11 <Nguyennhatanh.dev@gmail.com>
2024-10-10 17:16:13 +0700

    Delete Suggestion history

--- backend/src/entities/video-view-history.entity.ts
+++ backend/src/entities/video-view-history.entity.ts
@@ -4,12 +4,10 @@ import { BaseEntity } from './base/base.entity';
 
 @Entity('video_view_history')
 export class VideoViewHistorys extends BaseEntity {
-  // Liên kết với bảng Video (Nhiều lịch sử lượt xem thuộc về một video)
   @ManyToOne(() => Video, (video) => video.viewHistories, { onDelete: 'CASCADE' })
   video: Video;
 
-  // Ngày ghi nhận lượt xem của video
-  @Index() // Tạo index cho viewDate để tối ưu hóa các truy vấn liên quan đến ngày
+  @Index() 
   @Column({
     type: 'date',
   })
