9fd0cb3ba8bd62498db80a58a0330016ad3d1efe
AnhNN11 <Nguyennhatanh.dev@gmail.com>
2024-10-10 16:01:32 +0700

    3 API Search and 1 API Suggest

--- backend/src/entities/video.entity.ts
+++ backend/src/entities/video.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, Index, ManyToOne, OneToMany } from 'typeorm';
 import { BaseEntity } from './base/base.entity';
 import { WorkoutLevel } from './enums/workoutLevel.enum';
 import { DurationType } from './enums/durationType.enum';
@@ -9,28 +9,34 @@ import { Comment } from './comment.entity';
 import { Category } from './category.entity';
 import { Thumbnail } from './thumbnail.entity';
 import { Views } from './views.entity';
+import { VideoViewHistorys } from './video-view-history.entity';
 
 @Entity('videos')
 export class Video extends BaseEntity {
+  @Index() // Tạo index cho trường title để tối ưu tìm kiếm
   @Column({
     type: 'varchar',
+    length: 255,
   })
   title: string;
 
   @Column({
     type: 'enum',
     enum: WorkoutLevel,
+    default: WorkoutLevel.BEGINNER,
   })
   workoutLevel: WorkoutLevel;
 
   @Column({
     type: 'enum',
     enum: DurationType,
+    default: DurationType.LESS_30_MINS,
   })
   duration: DurationType;
 
   @Column({
     type: 'varchar',
+    length: 255,
     nullable: true,
   })
   keywords: string;
@@ -43,6 +49,7 @@ export class Video extends BaseEntity {
 
   @Column({
     type: 'varchar',
+    length: 255,
   })
   url: string;
 
@@ -64,18 +71,23 @@ export class Video extends BaseEntity {
   })
   numberOfComments: number;
 
+  // Liên kết với bảng Channel (Nhiều video thuộc về một Channel)
   @ManyToOne(() => Channel, (channel) => channel.videos)
   channel: Channel;
 
+  // Liên kết với bảng Comment (Một video có nhiều bình luận)
   @OneToMany(() => Comment, (comment) => comment.video)
   comments: Comment[];
 
+  // Liên kết với bảng Donation (Một video có thể có nhiều lượt donate)
   @OneToMany(() => Donation, (donation) => donation.video)
   donations: Donation[];
 
+  // Liên kết với bảng WatchingVideoHistory (Lưu lịch sử xem của video)
   @OneToMany(() => WatchingVideoHistory, (watchingVideoHistory) => watchingVideoHistory.video)
   watchingVideoHistories: WatchingVideoHistory[];
 
+  // Liên kết với bảng Category (Nhiều video thuộc một category)
   @ManyToOne(() => Category, (category) => category.videos)
   category: Category;
 
@@ -90,15 +102,24 @@ export class Video extends BaseEntity {
     default: 0,
   })
   shareCount: number;
+
+  // Liên kết với bảng Thumbnail (Một video có nhiều thumbnail)
   @OneToMany(() => Thumbnail, (thumbnail) => thumbnail.video)
   thumbnails: Thumbnail[];
 
   @Column({
     type: 'varchar',
     nullable: true,
+    length: 255,
   })
   urlS3: string;
 
   @OneToMany(() => Views, (view) => view.video)
   views: Views[];
+  // Liên kết với bảng VideoViewHistory (Một video có nhiều lịch sử lượt xem)
+  @OneToMany(() => VideoViewHistorys, (videoViewHistory) => videoViewHistory.video)
+  viewHistories: VideoViewHistorys[];
+
+  // Trường không được lưu vào cơ sở dữ liệu, chỉ tính toán tạm thời
+  viewGrowthRate?: number;
 }
--- backend/src/modules/search/dto/create-search.dto.ts
+++ backend/src/modules/search/dto/create-search.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSearchDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSearchHistoryDto {
+  @ApiProperty({
+    description: 'Content Search',
+    example: 'David',
+  })
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+}
