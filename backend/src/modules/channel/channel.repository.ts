362e51acacea3f9fe4c9ed25031075705525af22
Nhat Ninh <nguyennhatninh2212@gmail.com>
2024-10-17 10:03:42 +0700

    fix edit user not have channel

--- backend/src/modules/channel/channel.repository.ts
+++ backend/src/modules/channel/channel.repository.ts
@@ -8,7 +8,7 @@ export class ChannelRepository {
   constructor(@InjectRepository(Channel) private readonly channelRepository: Repository<Channel>) {}
 
   async getChannelByUserId(userId: number): Promise<Channel> {
-    return await this.channelRepository.findOneOrFail({
+    return await this.channelRepository.findOne({
       where: {
         user: { id: userId },
       },
