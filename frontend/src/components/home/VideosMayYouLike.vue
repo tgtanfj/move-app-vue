7c5b513c7a54f94ad6fef6018b1843ef1753d687
trungtan <70742166+tgtanfj@users.noreply.github.com>
2024-10-25 11:37:55 +0700

    done (#266)

--- frontend/src/components/home/VideosMayYouLike.vue
+++ frontend/src/components/home/VideosMayYouLike.vue
@@ -36,7 +36,7 @@ watch(
       <p class="font-bold text-[24px]">{{ $t('homepage.video_may_you_like') }}</p>
     </div>
     <div class="@container">
-      <div v-if="videos" class="grid grid-cols-3 gap-8 mt-4 @[1100px]:grid-cols-4">
+      <div v-if="videos" class="grid grid-cols-3 gap-8 mt-4 @[1200px]:grid-cols-4">
         <div v-for="(item, index) in videos" :key="index">
           <VideoMayLike :video="item" />
         </div>
