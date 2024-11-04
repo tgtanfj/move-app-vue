52c959bb07c6834f87695d8a06ebdc2b2a343c3d
hung1805 <70479829+hung1805@users.noreply.github.com>
2024-11-04 21:04:06 +0700

    [FE][BUG] Fix bug in add payment method (#381)
    * fix add payment method

--- frontend/src/components/NavbarLogged.vue
+++ frontend/src/components/NavbarLogged.vue
@@ -1,68 +1,3 @@
-<script setup>
-import DashboardIcon from '@assets/icons/DashboardIcon.vue'
-import AvaSystem from '@assets/icons/AvaSystem.vue'
-import LogoMoveMini from '@assets/icons/LogoMoveMini.vue'
-import defaultAvatar from '@assets/icons/default-avatar.png'
-import LogoutIcon from '@assets/icons/LogoutIcon.vue'
-import SettingIcon from '@assets/icons/SettingIcon.vue'
-import WalletIcon from '@assets/icons/WalletIcon.vue'
-import { Button } from '@common/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@common/ui/dropdown-menu'
-import { computed, ref, watch } from 'vue'
-import { RouterLink, useRoute, useRouter } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
-import BaseDialog from './BaseDialog.vue'
-import { videoService } from '@services/video.services'
-import Notification from '@components/notificataion/Notification.vue'
-import GetRep from './rep/GetRep.vue'
-import { usePaymentStore } from '../stores/payment'
-import BlueBadgeIcon from '@assets/icons/BlueBadgeIcon.vue'
-
-const props = defineProps({
-  isInStreamerPage: {
-    type: Boolean,
-    required: false
-  }
-})
-
-const authStore = useAuthStore()
-const paymentStore = usePaymentStore()
-const router = useRouter()
-const route = useRoute()
-const isStreamer = ref(route.path.startsWith('/streamer'))
-
-const showLogoutModal = ref(false)
-const userAvatar = ref(localStorage.getItem('userAvatar'))
-const storedUserInfo = localStorage.getItem('userInfo')
-const isBlueBadge = localStorage.getItem('userIsBlueBadge')
-const channelId = localStorage.getItem('userChannelId')
-
-const logOutGoogle = async () => {
-  await authStore.logout()
-  showLogoutModal.value = false
-  router.push('/')
-}
-
-const createChannel = async () => {
-  if (!route.path.startsWith('/streamer')) {
-    await videoService.createChannel()
-  }
-}
-
-watch(
-  () => route.path,
-  (newPath) => {
-    isStreamer.value = newPath.startsWith('/streamer')
-  }
-)
-</script>
-
 <template>
   <div class="flex items-center gap-3 h-full">
     <div class="mr-2">
@@ -71,44 +6,30 @@ watch(
     <Notification />
     <DropdownMenu>
       <DropdownMenuTrigger class="w-[30px] h-[30px]">
-        <img
-          :src="authStore.user.photoURL || authStore.user.avatar || userAvatar || defaultAvatar"
-          alt="Avatar"
-          class="w-[30px] h-[30px] object-cover rounded-full"
-        />
+        <img :src="avatar" alt="Avatar" class="w-[30px] h-[30px] object-cover rounded-full" />
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" class="mt-3 p-4 pt-5 w-[260px]">
-        <RouterLink
-          :to="`/channel/${authStore.user.channelId || authStore.channelId || channelId}`"
+        <DropdownMenuItem
+          class="flex items-center gap-1 p-0 pb-2 cursor-pointer group hover:text-primary focus:bg-transparent"
         >
-          <DropdownMenuItem
-            class="flex items-center gap-1 p-0 pb-2 cursor-pointer group hover:text-primary focus:bg-transparent"
+          <div class="w-[40px] h-[40px] rounded-full">
+            <img :src="avatar" alt="Avatar" class="w-full h-full rounded-full object-cover" />
+          </div>
+          <p
+            class="ml-2 font-semibold text-lg group-hover:text-primary duration-100 truncate w-[150px]"
           >
-            <div class="w-[40px] h-[40px] rounded-full">
-              <img
-                :src="
-                  authStore.user.photoURL || authStore.user.avatar || userAvatar || defaultAvatar
-                "
-                alt="Avatar"
-                class="w-full h-full rounded-full object-cover"
-              />
-            </div>
-            <p
-              class="ml-2 font-semibold text-lg group-hover:text-primary duration-100 truncate w-auto max-w-[150px]"
-            >
+            <router-link :to="`/channel/${authStore?.user?.channelId}`">
               {{
                 authStore.usernameUser ||
                 authStore.user.username ||
                 authStore.user?.data?.username ||
                 storedUserInfo
               }}
-            </p>
-            <BlueBadgeIcon
-              v-if="authStore.user.isBlueBadge || authStore.blueBadge || isBlueBadge"
-            />
-          </DropdownMenuItem>
-        </RouterLink>
+            </router-link>
+          </p>
+          <BlueBadgeIcon v-if="authStore.user.isBlueBadge" />
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
 
         <RouterLink
@@ -120,10 +41,8 @@ watch(
           <DropdownMenuItem
             class="flex gap-3 items-center py-2 px-0 cursor-pointer group hover:text-primary focus:bg-transparent"
           >
-             <LogoMoveMini class="group-hover:text-primary duration-100" />
-            <p class="font-semibold group-hover:text-primary duration-100">
-              Back to Move
-            </p>
+            <LogoMoveMini class="group-hover:text-primary duration-100" />
+            <p class="font-semibold group-hover:text-primary duration-100">Back to Move</p>
           </DropdownMenuItem>
         </RouterLink>
 
@@ -137,9 +56,7 @@ watch(
             class="flex gap-3 items-center py-2 px-0 cursor-pointer group hover:text-primary focus:bg-transparent"
           >
             <DashboardIcon class="group-hover:text-primary duration-100" />
-            <p class="font-semibold group-hover:text-primary duration-100">
-              Dashboard
-            </p>
+            <p class="font-semibold group-hover:text-primary duration-100">Dashboard</p>
           </DropdownMenuItem>
         </RouterLink>
 
@@ -152,9 +69,7 @@ watch(
             class="flex gap-3 items-center py-2 px-0 cursor-pointer group hover:text-primary focus:bg-transparent"
           >
             <WalletIcon class="group-hover:text-primary duration-100" />
-            <p class="font-semibold group-hover:text-primary duration-100">
-              Cashout
-            </p>
+            <p class="font-semibold group-hover:text-primary duration-100">Cashout</p>
           </DropdownMenuItem>
         </RouterLink>
 
@@ -213,3 +128,58 @@ watch(
     >
   </div>
 </template>
+
+<script setup>
+import DashboardIcon from '@assets/icons/DashboardIcon.vue'
+import defaultAvatar from '@assets/icons/default-avatar.png'
+import LogoutIcon from '@assets/icons/LogoutIcon.vue'
+import SettingIcon from '@assets/icons/SettingIcon.vue'
+import WalletIcon from '@assets/icons/WalletIcon.vue'
+import { Button } from '@common/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@common/ui/dropdown-menu'
+import { computed, ref, watch } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+import BaseDialog from './BaseDialog.vue'
+import { videoService } from '@services/video.services'
+import Notification from '@components/notificataion/Notification.vue'
+import GetRep from './rep/GetRep.vue'
+import { usePaymentStore } from '../stores/payment'
+import BlueBadgeIcon from '@assets/icons/BlueBadgeIcon.vue'
+
+const props = defineProps({
+  isInStreamerPage: {
+    type: Boolean,
+    required: false
+  }
+})
+
+const authStore = useAuthStore()
+const paymentStore = usePaymentStore()
+const router = useRouter()
+const showLogoutModal = ref(false)
+const storedUserInfo = localStorage.getItem('userInfo')
+const fetchLocalAvatar = () => {
+  return localStorage.getItem('userAvatar') || defaultAvatar
+}
+
+const avatar = computed(() => {
+  return authStore.user?.avatar || fetchLocalAvatar()
+})
+
+const logOutGoogle = async () => {
+  await authStore.logout()
+  showLogoutModal.value = false
+  router.push('/')
+}
+
+const createChannel = async () => {
+  await videoService.createChannel()
+}
+</script>
