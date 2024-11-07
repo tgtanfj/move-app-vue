31cbcdb2593a83c1b74fa48637b327fa3a68250c
DangHuan2k3 <huan.bk.0412@gmail.com>
2024-11-07 11:52:16 +0700

    chore: add more row in Admin

--- admin/app/dashboard/video-management/_components/video-listing-page.tsx
+++ admin/app/dashboard/video-management/_components/video-listing-page.tsx
@@ -1,21 +1,9 @@
 'use client';
 import PageContainer from '@/components/layout/page-container';
 import { Separator } from '@/components/ui/separator';
-import { useState } from 'react';
 import VideoTable from './video-tables';
-import { useVideoTableFilters } from './video-tables/use-video-table-filters';
 
 export default function VideoListingPage() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
-
-  const { searchQuery, workoutLevelFilter, setPage } = useVideoTableFilters();
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    setPage(page);
-  };
-
   return (
     <PageContainer scrollable>
       <div className="space-y-4">
