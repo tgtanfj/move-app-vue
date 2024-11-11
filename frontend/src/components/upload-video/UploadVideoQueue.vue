4017de0b4760a0955b3dcfa7494dbdb38c2213f1
trungtan <70742166+tgtanfj@users.noreply.github.com>
2024-11-11 11:27:30 +0700

    fixed (#450)

---
+++ frontend/src/components/upload-video/UploadVideoQueue.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Loading from '@components/Loading.vue'
+import { useVideoQueueStore } from '../../stores/videoQueue.store'
+import { CircleCheck } from 'lucide-vue-next'
+
+const videoQueueStore = useVideoQueueStore()
+</script>
+
+<template>
+  <div
+    v-if="videoQueueStore.isVideoQueueShow"
+    class="fixed bottom-0 right-0 m-4 w-[440px] bg-white shadow-lg rounded-lg overflow-hidden"
+  >
+    <div class="bg-primary text-white px-4 py-2 flex items-center justify-between">
+      <h3 class="font-semibold">Video Upload Queue</h3>
+    </div>
+
+    <div class="p-4 flex items-center space-x-4">
+      <div class="flex items-center w-full justify-start gap-4">
+        <div class="w-[30%] h-14 shrink-0">
+          <img class="w-full h-full object-cover shrink-0" :src="videoQueueStore.tempThumbnail" />
+        </div>
+        <div class="w-[70%] h-full flex gap-4 items-start justify-start">
+          <div class="flex w-[90%] flex-col gap-1 truncate">
+            <p class="text-black w-[230px] truncate font-semibold">
+              {{ videoQueueStore.tempVideoTitle }}
+            </p>
+            <div class="flex gap-2">
+              <div
+                class="h-[26px] rounded-full p-2 flex items-center justify-center cursor-pointer bg-primary text-white"
+              >
+                <p class="text-[11px] font-bold">{{ videoQueueStore.tempWorkoutLevel }}</p>
+              </div>
+              <div
+                class="h-[26px] rounded-full p-2 flex items-center justify-center cursor-pointer bg-primary text-white"
+              >
+                <p class="text-[11px] font-bold">{{ videoQueueStore.tempDuration }}</p>
+              </div>
+            </div>
+          </div>
+          <div
+            v-if="videoQueueStore.uploadLoadingVideoQueue"
+            class="h-[20px] w-[10%] ml-auto my-auto"
+          >
+            <Loading />
+          </div>
+          <div v-else class="h-[20px] w-[10%] ml-auto my-auto">
+            <CircleCheck stroke-width="3" class="text-primary w-[18px] h-[18px] font-semibold" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box-shadow {
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 4px 16px;
+}
+</style>
---
+++ frontend/src/stores/videoQueue.store.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useVideoQueueStore = defineStore('video_queue', () => {
+  const isOpenVideoQueue = ref(true)
+  const isVideoQueueShow = ref(false)
+  const countdownVideoQueue = ref(5)
+  const isCountingVideoQueue = ref(false)
+  const tempThumbnail = ref(null)
+  const tempVideoTitle = ref(null)
+  const tempWorkoutLevel = ref(null)
+  const tempDuration = ref(null)
+  const isBannedUpload = ref(false)
+  const uploadLoadingVideoQueue = ref(false)
+
+  const toggleIsOpenVideoQueue = () => {
+    isOpenVideoQueue.value = !isOpenVideoQueue.value
+  }
+  return {
+    isOpenVideoQueue,
+    isVideoQueueShow,
+    countdownVideoQueue,
+    isCountingVideoQueue,
+    tempThumbnail,
+    tempVideoTitle,
+    tempWorkoutLevel,
+    tempDuration,
+    isBannedUpload,
+    uploadLoadingVideoQueue,
+    toggleIsOpenVideoQueue,
+  }
+})
